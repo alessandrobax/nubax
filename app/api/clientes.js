ce17b5a6525026b0cad2e2488f27b5d460377868
Alessandro Bax de Barros Aquino <Alessandro.Aquino@ibm.com>
2018-08-10 15:39:27 -0300

    ajustes interface
    .

--- app/api/clientes.js
+++ app/api/clientes.js
@@ -4,7 +4,12 @@ var api = {};
 
 var clientes = [
     {_id: 1, nome: 'Ana', idade: 20, endereco: "Rua XPTO", conta: 001, saldo: 500},
-    {_id: 1, nome: 'Pedro', idade: 47, endereco: "Rua Dois", conta: 002, saldo: 7500}
+    {_id: 2, nome: 'Pedro', idade: 47, endereco: "Rua Dois", conta: 002, saldo: 7500},
+    {_id: 3, nome: 'João', idade: 34, endereco: "Rua Cinco", conta: 003, saldo: 565},
+    {_id: 4, nome: 'Lucas', idade: 87, endereco: "Rua Raul", conta: 004, saldo: 4576},
+    {_id: 5, nome: 'Maria', idade: 55, endereco: "Rua Fontana", conta: 005, saldo: 550},
+    {_id: 6, nome: 'Jorge', idade: 23, endereco: "Rua Rio", conta: 006, saldo: 3487},
+    {_id: 7, nome: 'Antônio', idade: 418, endereco: "Rua Guajajaras", conta: 007, saldo: 45}
     
 ];
 
